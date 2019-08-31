85799c0738542f2c4d786ef6c8f3abfd369ba334
davidmorosini <david.morosinineto@gmail.com>
2019-08-31 17:44:45 -0300

    biblioteca base para listas encadiadas

---
+++ utils/linkedlist.c
@@ -0,0 +1,103 @@
+#include "linkedlist.h"
+
+/*Funções para inicialização das estruturas de dados*/
+lst_node * new_node_(cell * data, lst_node * prox_node){
+    lst_node * n = (lst_node *)malloc(sizeof(lst_node));
+    assert(n != NULL);
+    if(n != NULL){
+        n -> data = data;
+        n -> prox = prox_node;
+    }
+    return n;
+}
+
+lst_node * new_node(cell * data){
+    lst_node * n = (lst_node *)malloc(sizeof(lst_node));
+    assert(n != NULL);
+    if(n != NULL){
+        n -> data = data;
+        n -> prox = NULL;
+    }
+    return n;
+}
+
+
+lst * new_lst(lst_node * start){
+    lst * l = (lst *)malloc(sizeof(lst));
+    assert(l != NULL);
+    if(l != NULL){
+        l -> start = start;
+        l -> end = start;
+    }
+    return l;
+}
+
+lst * add_init(lst * l, lst_node * n){
+    if(l == NULL || l -> start == NULL){
+        l = new_lst(n);
+    }else{
+        n -> prox = l -> start;
+        l -> start = n;
+    }
+    return l;
+}
+
+lst * add_end(lst * l, lst_node * n){
+    if(l == NULL || l -> start == NULL){
+        l = new_lst(n);
+    }else{
+        l -> end -> prox = n;
+        l -> end = n;
+    }
+    return l;
+}
+
+cell * get_data_node(lst_node * l){
+    cell * data;
+    if(l != NULL){
+        data = l -> data;
+    }
+    return data;
+}
+
+lst_node * get_prox_node(lst_node * l){
+    lst_node * lp = NULL;
+    assert(l != NULL);
+    if(l != NULL){
+        lp = l -> prox;
+    }
+    return lp;
+}
+
+lst_node * get_start_node(lst * l){
+    lst_node * ls = NULL;
+    if(l != NULL){
+        ls = l -> start;
+    }
+    return ls;    
+}
+
+lst_node * get_end_node(lst * l){
+    lst_node * le = NULL;
+    if(l != NULL){
+        le = l -> end;
+    }
+    return le;
+}
+
+int get_size_lst(lst * l){
+    int size = 0;
+    assert(l != NULL);
+    if(l != NULL && l -> start != NULL){
+        size = 1;
+        //se o comeco for igual ao fim, significa que só tem um
+        if(l -> start != l-> end){
+            lst_node * aux = l -> start -> prox;
+            while(aux != NULL){
+                size++;
+                aux = aux -> prox;
+            }
+        }
+    }
+    return size;
+}
---
+++ utils/linkedlist.h
@@ -0,0 +1,43 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+
+//trocando para um formato genérico
+typedef void cell;
+/*Célula de conteúdo para cada nó da lista*/
+//typedef struct cell{
+//    int num;
+//}cell;
+
+/*Estrutura de dados para um nó da lista*/
+typedef struct node{
+   cell * data;
+   struct node * prox;
+}lst_node;
+
+/* Estrutura de dados para uma lista com centinela*/
+typedef struct linkedlist{
+    lst_node * start, * end;
+}lst;
+
+/*Funções para inicialização das estruturas de dados*/
+lst_node * new_node_(cell * data, lst_node * prox_node);
+lst_node * new_node(cell * data);
+lst * new_lst(lst_node * start);
+
+lst * add_init(lst * l, lst_node * n);
+lst * add_end(lst * l, lst_node * n);
+cell * get_data_node(lst_node * l);
+
+lst_node * get_prox_node(lst_node * l);
+lst_node * get_start_node(lst * l);
+lst_node * get_end_node(lst * l);
+
+int get_size_lst(lst * l);
+
+
+#endif
