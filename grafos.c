1def6b87467d463a73bccaa95ade6a8e9794b784
davidmorosini <david.morosinineto@gmail.com>
2019-08-31 17:56:56 -0300

    alterando o arquivo de exemplo para o novo formato de adição de vertices

--- grafos.c
+++ grafos.c
@@ -5,10 +5,10 @@
 
 int main(int argc, char * argv[]){
     
-    graph * g = new_graph(new_data_node(1));
-    g = add_vertex(g, new_data_node(2));
-    g = add_vertex(g, new_data_node(3));
-    g = add_vertex(g, new_data_node(4));
+    graph * g = new_graph(1);
+    g = add_vertex(g, 2);
+    g = add_vertex(g, 3);
+    g = add_vertex(g, 4);
 
     g = add_new_edge(g, 1, 2);
     
